91def1c1564ebd7d807a4f7aa59f6bfdc00581a7
aptus0 <aptus0@users.noreply.github.com>
2026-02-22 18:12:35 +0300

    Initial Commit

---
+++ CleanMacOS/CleanMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanMacOSApp.swift
+//  CleanMacOS
+//
+//  Created by Samet on 22.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct CleanMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
